e855dbdc14f0d0263408dd49bb04b95c5c40e726
Lel010 <averageguytooversatile@gmail.com>
2023-04-07 01:46:28 +0800

    Updates

---
+++ precache-manifest.366228fbd8275ba7671cc48e508e668e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "84d0c8c4e60eae9f6e64",
+    "url": "/world-clock-bootcamp/static/css/main.07fc4028.chunk.css"
+  },
+  {
+    "revision": "84d0c8c4e60eae9f6e64",
+    "url": "/world-clock-bootcamp/static/js/main.84d0c8c4.chunk.js"
+  },
+  {
+    "revision": "46bf9cab5e2b2e94e692",
+    "url": "/world-clock-bootcamp/static/css/1.5b2af2a2.chunk.css"
+  },
+  {
+    "revision": "46bf9cab5e2b2e94e692",
+    "url": "/world-clock-bootcamp/static/js/1.46bf9cab.chunk.js"
+  },
+  {
+    "revision": "ff92a1879741f3521e76",
+    "url": "/world-clock-bootcamp/static/js/runtime~main.ff92a187.js"
+  },
+  {
+    "revision": "f5c8d3937ba7a8aa14136ed2b37b0874",
+    "url": "/world-clock-bootcamp/static/media/logo.f5c8d393.png"
+  },
+  {
+    "revision": "4b288c843a259477484f93f60fa00f4e",
+    "url": "/world-clock-bootcamp/index.html"
+  }
+];
